378fa3a07124a009784b89ad19642fc04f9c4bbd
Ragavi <25cs196@kpriet.ac.in>
2026-08-19 20:45:15 +0530

    Change Order class fields to private and update methods

--- Restaurant/Order.java
+++ Restaurant/Order.java
@@ -1,19 +1,19 @@
 package Restaurant;
 
 public class Order {
-    int orderId;
-    String customerName;
-    String foodName;
-    int quantity;
+    private int orderId;
+    private String customerName;
+    private String foodName;
+    private int quantity;
 
-    Order(int orderId, String customerName, String foodName, int quantity) {
+    public Order(int orderId, String customerName, String foodName, int quantity) {
         this.orderId = orderId;
         this.customerName = customerName;
         this.foodName = foodName;
         this.quantity = quantity;
     }
 
-    void displayOrder() {
+    public void displayOrder() {
         System.out.println("Order ID: " + orderId);
         System.out.println("Customer Name: " + customerName);
         System.out.println("Food Name: " + foodName);
