7709e34568fb14b7a5026b4508232fd438cb7383
Ragavi <25cs196@kpriet.ac.in>
2026-08-19 20:44:28 +0530

    Update Restaurant.java

--- Restaurant/Restaurant.java
+++ Restaurant/Restaurant.java
@@ -1,17 +1,17 @@
 package Restaurant;
 
 public class Restaurant {
-    String restaurantName;
-    String foodName;
-    int price;
+    private String restaurantName;
+    private String foodName;
+    private int price;
 
-    Restaurant(String restaurantName, String foodName, int price) {
+    public Restaurant(String restaurantName, String foodName, int price) {
         this.restaurantName = restaurantName;
         this.foodName = foodName;
         this.price = price;
     }
 
-    void displayOrder() {
+    public void displayOrder() {
         System.out.println("Restaurant: " + restaurantName);
         System.out.println("Food: " + foodName);
         System.out.println("Price: " + price);
