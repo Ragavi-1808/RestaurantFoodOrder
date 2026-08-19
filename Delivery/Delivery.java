36343ecf42589cbd2577662eefd2985a666c0e0b
Ragavi <25cs196@kpriet.ac.in>
2026-08-19 20:45:59 +0530

    Update Delivery.java

--- Delivery/Delivery.java
+++ Delivery/Delivery.java
@@ -1,17 +1,17 @@
 package Delivery;
 
 public class Delivery {
-    String deliveryPerson;
-    String address;
-    String status;
+    private String deliveryPerson;
+    private String address;
+    private String status;
 
-    Delivery(String deliveryPerson, String address, String status) {
+    public Delivery(String deliveryPerson, String address, String status) {
         this.deliveryPerson = deliveryPerson;
         this.address = address;
         this.status = status;
     }
 
-    void displayDelivery() {
+    public void displayDelivery() {
         System.out.println("Delivery Person: " + deliveryPerson);
         System.out.println("Address: " + address);
         System.out.println("Status: " + status);
